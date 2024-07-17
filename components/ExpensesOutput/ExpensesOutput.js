8f7730c452176b40328d703217c09f4f2916a3ce
AliveKiwi <prateekjais.dev@gmail.com>
2024-07-17 14:19:29 +0530

    133 style: added styling in ExpensesOutput and ExpensesSummary

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -4,6 +4,8 @@ import { StyleSheet, View } from 'react-native';
 import ExpensesSummary from './ExpensesSummary';
 import ExpensesList from './ExpensesList';
 
+import { GlobalStyles } from '../../constants/styles';
+
 const DUMMY_EXPENSES = [
   {
     id: 'e1',
@@ -40,7 +42,8 @@ const DUMMY_EXPENSES = [
 // 130 added props expenses, expensesPeriod through destructuring
 export default function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
-    <View>
+    // 133 added style
+    <View style={styles.container}>
       {/* 
       130 passing props to ExpensesSummary for destructuring 
       131 passing DUMMY_EXPENSES to ExpensesSummary
@@ -52,4 +55,11 @@ export default function ExpensesOutput({ expenses, expensesPeriod }) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  // 133 added backgroundColor to parent View
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+});
--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -2,6 +2,8 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
+import { GlobalStyles } from '../../constants/styles';
+
 // 130 added props expenses, periodName through destructuring
 export default function ExpensesSummary({ expenses, periodName }) {
   // 130 total the expenses into single value expensesSum
@@ -10,11 +12,33 @@ export default function ExpensesSummary({ expenses, periodName }) {
   }, 0);
 
   return (
-    <View>
-      <Text>Last 7 Days</Text>
-      <Text>${expensesSum.toFixed(2)}</Text>
+    // 133 added style container to parent View, and period, sum to child Text
+    <View style={styles.container}>
+      <Text style={styles.period}>Last 7 Days</Text>
+      <Text style={styles.sum}>${expensesSum.toFixed(2)}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  // 133 made parent View into header with split text
+  container: {
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary50,
+    borderRadius: 6,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  // 133 changing font size and text color of Text where style is period
+  period: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary400,
+  },
+  // 133 changing font size and text color of Text where style is sum
+  sum: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: GlobalStyles.colors.primary500,
+  },
+});
