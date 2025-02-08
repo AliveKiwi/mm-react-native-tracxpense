e96bd30b462e7349e366c499e9a05bd7f6b33137
AliveKiwi <prateekjais.dev@gmail.com>
2025-02-08 17:47:41 +0530

    140 functionality: added Delete button to Edit Expense

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,6 +1,8 @@
 // 127 created
 import { StyleSheet, Text, View } from 'react-native';
 import React, { useLayoutEffect } from 'react';
+import { GlobalStyles } from '../constants/styles';
+import IconButton from '../components/UI/IconButton';
 
 export default function ManageExpenses({ route, navigation }) {
   // 139 added route
@@ -16,11 +18,38 @@ export default function ManageExpenses({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
+  // 140 added deleteExpenseHandler
+  function deleteExpenseHandler() {}
+
+  // 140 added View
   return (
-    <View>
-      <Text>ManageExpenses</Text>
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+// 140 added container and deleteContainer styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
+});
